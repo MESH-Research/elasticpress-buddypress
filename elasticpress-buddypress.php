6f6b08636737e3621817b2bd2782a59db591a851
Ryan Williams <ryrowi@gmail.com>
2017-04-20 15:27:23 -0400

    css, templates, additional filters for results

--- elasticpress-buddypress.php
+++ elasticpress-buddypress.php
@@ -10,15 +10,43 @@ require_once dirname( __FILE__ ) . '/features/buddypress/buddypress.php';
 require_once dirname( __FILE__ ) . '/classes/class-ep-bp-api.php';
 
 /**
- * Register ElasticPress custom feature
- */
-add_action( 'bp_init', 'ep_bp_register_feature' );
-
-/**
  * Sync BP content after EP has synced posts
+ * TODO dashboard and other sync contexts besides cli?
  */
 add_action( 'ep_cli_post_bulk_index', 'ep_bp_bulk_index_groups' );
 //add_action( 'ep_cli_post_bulk_index', 'ep_bp_bulk_index_members' );
+
+/**
+ * Initialize custom feature etc.
+ */
+function ep_bp_init() {
+	ep_bp_register_feature();
+}
+add_action( 'bp_init', 'ep_bp_init' );
+
+/**
+ * use our own search template to accommodate groups & members as well as posts.
+ * default search template assumes everything is a post. we need to support other contents types too.
+ */
+function ep_bp_filter_template_include($template){
+	global $wp_query;
+
+	if ( $wp_query->is_search ) {
+		$template = dirname( __FILE__ ) . '/templates/search.php';
+	}
+
+	return $template;
+}
+//add_filter( 'template_include', 'ep_bp_filter_template_include' );
+
+/**
+ * styles to clean up search results
+ */
+function ep_bp_enqueue_style() {
+	wp_register_style( 'elasticpress-buddypress', plugins_url( '/elasticpress-buddypress/css/elasticpress-buddypress.css' ) );
+	wp_enqueue_style( 'elasticpress-buddypress' );
+}
+add_action( 'wp_enqueue_scripts', 'ep_bp_enqueue_style' );
 
 /**
  * Filter search request path to search groups & members as well as posts.
@@ -31,17 +59,17 @@ add_filter( 'ep_search_request_path', 'ep_bp_filter_ep_search_request_path' );
 /**
  * Filter search request post_filter post_type to search groups & members as well as posts.
  * These aren't real post types in WP, but they are in EP because of the way EP_BP_API indexes.
+ * TODO doesn't work. when post_type is in the filter, no results are returned regardless of what types we pass.
+ * disable post_type filter instead for now.
  */
 function ep_bp_filter_ep_searchable_post_types( $post_types ) {
 	return array_unique( array_merge( $post_types, [ 'group', 'member' ] ) );
 }
-// TODO this does not work. when post_type is in the filter, no results are returned regardless of what types we pass.
-// disable post_type filter instead for now.
 //add_filter( 'ep_searchable_post_types', 'ep_bp_filter_ep_searchable_post_types' );
 
 /**
- * Remove post_type filter.
- * This can be removed when ep_bp_filter_ep_searchable_post_types() is fixed.
+ * Remove post_type filter for search queries.
+ * This is a workaround until ep_bp_filter_ep_searchable_post_types() is fixed.
  */
 function ep_bp_filter_ep_formatted_args( $formatted_args ) {
 	foreach ( $formatted_args['post_filter']['bool']['must'] as $i => $must ) {
@@ -51,3 +79,17 @@ function ep_bp_filter_ep_formatted_args( $formatted_args ) {
 	}
 }
 add_filter( 'ep_formatted_args', 'ep_bp_filter_ep_formatted_args' );
+
+/**
+ * Filter the search results loop to fix non-post (groups, members) permalinks.
+ */
+function ep_bp_filter_the_permalink( $permalink ) {
+	global $wp_query, $post;
+
+	if ( $wp_query->is_search && in_array( $post->post_type,  [ 'group', 'member' ] ) ) {
+		$permalink = $post->permalink;
+	}
+
+	return $permalink;
+}
+add_filter( 'the_permalink', 'ep_bp_filter_the_permalink' );
