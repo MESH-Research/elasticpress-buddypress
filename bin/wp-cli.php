646fbff0b0c7a2319faf38b74ef8b49cd1888cba
Ryan Williams <ryrowi@gmail.com>
2017-04-25 10:19:47 -0400

    allow independent bulk indexing of groups and members with cli args

--- bin/wp-cli.php
+++ bin/wp-cli.php
@@ -10,10 +10,17 @@
 class ElasticPress_BuddyPress_CLI_Command extends WP_CLI_Command {
 
 	public function index( $args, $assoc_args ) {
-		WP_CLI::line( 'Indexing groups...' );
-		ep_bp_bulk_index_groups();
-		WP_CLI::line( 'Indexing members...' );
-		ep_bp_bulk_index_members();
+
+		if ( ! isset( $args[0] ) || 'groups' === $args[0] ) {
+			WP_CLI::line( 'Indexing groups...' );
+			ep_bp_bulk_index_groups();
+		}
+
+		if ( ! isset( $args[0] ) || 'members' === $args[0] ) {
+			WP_CLI::line( 'Indexing members...' );
+			ep_bp_bulk_index_members();
+		}
+
 	}
 
 }
